0edb9f7328b54a69bf68e6de355c7b931f6d0cbc
tomas sopranzetti <sopraaan@gmail.com>
2022-05-22 17:54:34 -0300

    agregando carrito

--- Cuotas.js
+++ Cuotas.js
@@ -1,21 +1,46 @@
 let valor;
 let cuotas;
+let precioTeclado = 10000;
+let PrecioPad = 1500;
+let precioMouse = 5000;
 
-function calculadora(valor, cuotas) {
-  return (valor / cuotas) * 1.0425;
+carrito = [];
+// se arma una funcion que tome el producto ingresado por el usuario, obteniendo su precio y sumando el total del carrito
+function sumacarrito() {
+  let precioTotal = 0;
+  for (i = 0; i < carrito.length; i++) {
+    precioTotal += carrito[i];
+  }
+  return precioTotal;
 }
 
+// el usuario ve los productos y valores
+alert(  "teclado " +    "$" +    precioTeclado +    "\n" +    "pad " +    "$" +    PrecioPad +    "\n" +    "mouse " +    "$" +    precioMouse
+);
 
+//usuario escriben en prompt el producto que desea comprar, y se deposita en un array para calcular total del compra
 do {
-  valor = prompt("ingrese un valor para cuotificar");
+  productos = prompt("produco a comprar");
 
-  if (isNaN(valor)) {
-    alert("Ingresa valor");
+  if (productos === "teclado") {
+    carrito.push(precioTeclado);
+    alert("para finalizar compra, esribir 0");
   }
-} while (isNaN(valor) || valor <= 0);
+  if (productos === "pad") {
+    carrito.push(precioPad);
+    alert("para finalizar compra, esribir 0");
+  }
+  if (productos === "mouse") {
+    carrito.push(precioMouse);
+    alert("para finalizar compra, esribir 0");
+  }
+} while (productos != 0);
 
-// le pedimos a usuario un valor numerico distintoi de 0, de no ser asi vuelve a solicitar
+let total = sumacarrito();
 
+function calculadora(total, cuotas) {
+  return (total / cuotas) * 1.0425;
+}
 
 do {
   cuotas = prompt("las cuotas deseadas");
@@ -26,5 +51,7 @@ do {
 
   // le pedimos a usuario un valor numerico distintoi de 0, de no ser asi vuelve a solicitar
 } while (isNaN(cuotas) || cuotas < 1);
-let precio = calculadora(valor, cuotas);
+console.log(total);
+console.log(cuotas);
+let precio = calculadora(total, cuotas);
 alert(precio);
